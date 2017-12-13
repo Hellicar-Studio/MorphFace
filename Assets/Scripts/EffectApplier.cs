a766783a2c83fd704b93da9ac47e20f6e4753b33
Bentleyj <jgbentley10@gmail.com>
2017-12-13 11:48:20 +0000

    Add effect applier

---
+++ Assets/Scripts/EffectApplier.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectApplier : MonoBehaviour {
+
+	public Material effect;
+
+	protected void nRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		if(effect != null)
+		{
+			Graphics.Blit(source, destination, effect);
+		} else
+		{
+			Graphics.Blit(source, destination);
+		}
+	}
+}
