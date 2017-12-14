5cacff2746ed8a8bdc15f20e4cfab60244cc3de8
Bentleyj <jgbentley10@gmail.com>
2017-12-14 11:46:13 +0000

    Add Max Distort limit to gui

--- Assets/Scripts/GuiController.cs
+++ Assets/Scripts/GuiController.cs
@@ -8,20 +8,29 @@ public class GuiController : MonoBehaviour {
 	public Material mat;
 
 	public Slider sMin;
-	public Slider sMax;
+	public Slider maxDistort;
+
+	public GameObject controls;
 
 	// Use this for initialization
 	void Start () {
 		sMin.value = PlayerPrefs.GetFloat("_StrengthMin");
-		sMax.value = PlayerPrefs.GetFloat("_StrengthMax");
+		Cursor.visible = false;
+		maxDistort.value = PlayerPrefs.GetFloat("_MaxDistort");
 	}
 
 	// Update is called once per frame
 	void Update () {
 		PlayerPrefs.SetFloat("_StrengthMin", sMin.value);
-		PlayerPrefs.SetFloat("_StrengthMax", sMax.value);
+		PlayerPrefs.SetFloat("_MaxDistort", maxDistort.value);
 
 		mat.SetFloat("_StrengthMin", sMin.value);
-		mat.SetFloat("_StrengthMax", sMax.value);
+		mat.SetFloat("_MaxDistort", maxDistort.value);
+
+		if(Input.GetKeyUp("g"))
+		{
+			controls.SetActive(!controls.active);
+			Cursor.visible = controls.active;
+		}
 	}
 }
